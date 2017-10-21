dc7f7a045f9b499c0e5e8a43b3727e7e851f0360
szbence26 <szokebenc@gmail.com>
2017-10-21 16:45:47 +0200

    Projekt kezdete

---
+++ RegistrationPlateRecognizer/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace RegistrationPlateRecognizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recognize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cannyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // recognize
+            // 
+            this.recognize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.recognize.Location = new System.Drawing.Point(101, 86);
+            this.recognize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.recognize.Name = "recognize";
+            this.recognize.Size = new System.Drawing.Size(164, 117);
+            this.recognize.TabIndex = 0;
+            this.recognize.Text = "recognize";
+            this.recognize.UseVisualStyleBackColor = true;
+            this.recognize.Click += new System.EventHandler(this.recognize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 297);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // cannyButton
+            // 
+            this.cannyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cannyButton.Location = new System.Drawing.Point(304, 86);
+            this.cannyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cannyButton.Name = "cannyButton";
+            this.cannyButton.Size = new System.Drawing.Size(164, 117);
+            this.cannyButton.TabIndex = 2;
+            this.cannyButton.Text = "Canny";
+            this.cannyButton.UseVisualStyleBackColor = true;
+            this.cannyButton.Click += new System.EventHandler(this.cannyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 596);
+            this.Controls.Add(this.cannyButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recognize);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button recognize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cannyButton;
+    }
+}
+
---
+++ RegistrationPlateRecognizer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace RegistrationPlateRecognizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Mat WhiteImage;
+        Mat canny;
+
+        private void recognize_Click(object sender, EventArgs e)
+        {
+            Cv2.NamedWindow("kép");
+            Cv2.ResizeWindow("kép", 1280, 720);
+
+            Mat image = new Mat(@"D:\Visual_studio\RegistrationPlateRecognizer\car.jpg");
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+            WhiteImage = new Mat();
+            //Cv2.InRange(image, new Scalar(160, 160, 160), new Scalar(255, 255, 255), WhiteImage);
+
+            Cv2.Threshold(grayImage, WhiteImage, 190, 255, ThresholdTypes.Binary);
+            Dilate(WhiteImage, 2);
+
+            //kontúrok megtalálsa
+            OpenCvSharp.Point[][] contours;
+            HierarchyIndex[] hierarchy;
+            Cv2.FindContours(WhiteImage, out contours, out hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+             
+            /// Draw contours and find biggest contour (if there are other contours in the image, we assume the biggest one is the desired rect)
+            // drawing here is only for demonstration!
+            int biggestContourIdx = -1;
+            float biggestContourArea = 0;
+            Mat drawing = Mat.Zeros(WhiteImage.Size(), MatType.CV_8UC3);
+            List<OpenCvSharp.Point[]> contoursList = contours.ToList();
+            for (int i = 0; i < contours; i++)
+            {
+                cv::Scalar color = cv::Scalar(0, 100, 0);
+                drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point());
+                Cv2.DrawContours(drawing, )
+
+                float ctArea = cv::contourArea(contours[i]);
+                if (ctArea > biggestContourArea)
+                {
+                    biggestContourArea = ctArea;
+                    biggestContourIdx = i;
+                }
+            }
+
+            // if no contour found
+            if (biggestContourIdx < 0)
+            {
+                std::cout << "no contour found" << std::endl;
+                return 1;
+            }
+
+            // compute the rotated bounding rect of the biggest contour! (this is the part that does what you want/need)
+            cv::RotatedRect boundingBox = cv::minAreaRect(contours[biggestContourIdx]);
+            // one thing to remark: this will compute the OUTER boundary box, so maybe you have to erode/dilate if you want something between the ragged lines
+
+
+
+            // draw the rotated rect
+            cv::Point2f corners[4];
+            boundingBox.points(corners);
+            cv::line(drawing, corners[0], corners[1], cv::Scalar(255, 255, 255));
+            cv::line(drawing, corners[1], corners[2], cv::Scalar(255, 255, 255));
+            cv::line(drawing, corners[2], corners[3], cv::Scalar(255, 255, 255));
+            cv::line(drawing, corners[3], corners[0], cv::Scalar(255, 255, 255));
+
+            // display
+            cv::imshow("input", input);
+            cv::imshow("drawing", drawing);
+            cv::waitKey(0);
+
+            cv::imwrite("rotatedRect.png", drawing);
+            //OpenCvSharp.Point[][] contours = Cv2.FindContoursAsArray(WhiteImage, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+
+            Cv2.ImShow("kép", WhiteImage);
+        }
+
+        void Dilate(Mat image, int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Cv2.Dilate(image, image, new Mat());
+            }
+        }
+
+        private void cannyButton_Click(object sender, EventArgs e)
+        {
+            Cv2.NamedWindow("canny");
+            Cv2.ResizeWindow("canny", 1280, 720);
+            canny = new Mat();
+            Cv2.Canny(WhiteImage, canny, 1000, 500);
+            Dilate(canny, 1);
+            Cv2.ImShow("canny", canny);
+        }
+    }
+}
