533d466b75f86a743a647ce32e22397161ce80ee
szbence26 <szokebenc@gmail.com>
2017-11-01 18:24:16 +0100

    Rendszámtábla kitakarása, picturebox létrehozása

--- RegistrationPlateRecognizer/Form1.Designer.cs
+++ RegistrationPlateRecognizer/Form1.Designer.cs
@@ -29,65 +29,61 @@
         private void InitializeComponent()
         {
             this.recognize = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cannyButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.show = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // recognize
             // 
             this.recognize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.recognize.Location = new System.Drawing.Point(101, 86);
-            this.recognize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.recognize.Location = new System.Drawing.Point(76, 194);
             this.recognize.Name = "recognize";
-            this.recognize.Size = new System.Drawing.Size(164, 117);
+            this.recognize.Size = new System.Drawing.Size(123, 95);
             this.recognize.TabIndex = 0;
             this.recognize.Text = "recognize";
             this.recognize.UseVisualStyleBackColor = true;
             this.recognize.Click += new System.EventHandler(this.recognize_Click);
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 297);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.pictureBox1.Location = new System.Drawing.Point(246, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1071, 644);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
-            // cannyButton
+            // show
             // 
-            this.cannyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cannyButton.Location = new System.Drawing.Point(304, 86);
-            this.cannyButton.Margin = new System.Windows.Forms.Padding(4);
-            this.cannyButton.Name = "cannyButton";
-            this.cannyButton.Size = new System.Drawing.Size(164, 117);
-            this.cannyButton.TabIndex = 2;
-            this.cannyButton.Text = "Canny";
-            this.cannyButton.UseVisualStyleBackColor = true;
-            this.cannyButton.Click += new System.EventHandler(this.cannyButton_Click);
+            this.show.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.show.Location = new System.Drawing.Point(76, 70);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(123, 95);
+            this.show.TabIndex = 2;
+            this.show.Text = "Show Picture";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 596);
-            this.Controls.Add(this.cannyButton);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1358, 726);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.recognize);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button recognize;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button cannyButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button show;
     }
 }
 
--- RegistrationPlateRecognizer/Form1.cs
+++ RegistrationPlateRecognizer/Form1.cs
@@ -19,79 +19,79 @@ namespace RegistrationPlateRecognizer
         }
 
         Mat WhiteImage;
-        Mat canny;
+        string path = @"D:\Visual_studio\RegistrationPlateRecognizer\Pictures\car2.jpg";
 
         private void recognize_Click(object sender, EventArgs e)
         {
-            Cv2.NamedWindow("kép");
-            Cv2.ResizeWindow("kép", 1280, 720);
 
-            Mat image = new Mat(@"D:\Visual_studio\RegistrationPlateRecognizer\car.jpg");
+            Mat image = new Mat(path);
             Mat grayImage = new Mat();
             Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
             WhiteImage = new Mat();
-            //Cv2.InRange(image, new Scalar(160, 160, 160), new Scalar(255, 255, 255), WhiteImage);
 
-            Cv2.Threshold(grayImage, WhiteImage, 190, 255, ThresholdTypes.Binary);
-            Dilate(WhiteImage, 2);
+            Cv2.Threshold(grayImage, WhiteImage, 180, 255, ThresholdTypes.Binary);
+            Erode(WhiteImage, 1);
 
             //kontúrok megtalálsa
             OpenCvSharp.Point[][] contours;
             HierarchyIndex[] hierarchy;
             Cv2.FindContours(WhiteImage, out contours, out hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            
 
-             
-            /// Draw contours and find biggest contour (if there are other contours in the image, we assume the biggest one is the desired rect)
-            // drawing here is only for demonstration!
-            int biggestContourIdx = -1;
-            float biggestContourArea = 0;
-            Mat drawing = Mat.Zeros(WhiteImage.Size(), MatType.CV_8UC3);
-            List<OpenCvSharp.Point[]> contoursList = contours.ToList();
-            for (int i = 0; i < contours; i++)
+            var componentCount = 0;
+            var contourIndex = 0;
+            Mat drawing = Mat.Zeros(WhiteImage.Size(), MatType.CV_8UC1);
+            while (contourIndex >= 0)
             {
-                cv::Scalar color = cv::Scalar(0, 100, 0);
-                drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point());
-                Cv2.DrawContours(drawing, )
+                Cv2.DrawContours(drawing, contours, contourIndex, Scalar.All(componentCount + 15), -1, LineTypes.Link8, hierarchy, int.MaxValue);
+                componentCount++;
+                contourIndex = hierarchy[contourIndex].Next;
+            }
+            Erode(drawing, 10);
+            Dilate(drawing, 10);
 
-                float ctArea = cv::contourArea(contours[i]);
-                if (ctArea > biggestContourArea)
+            Cv2.ImWrite(@"D:\Visual_studio\RegistrationPlateRecognizer\Pictures\car_Drawing.jpg", drawing);
+            Rect plate = new Rect();
+            bool find = false;
+
+            for (int y = image.Height - 1; y >= 0; y--)
+            {
+                for (int x = image.Width - 1; x >= 0; x--)
                 {
-                    biggestContourArea = ctArea;
-                    biggestContourIdx = i;
+                    if(drawing.At<byte>(y, x) != 0)
+                    {
+                        Cv2.FloodFill(drawing, new OpenCvSharp.Point(x, y), new Scalar(255), out plate, null, null, 4);
+                        find = true;
+                        break;
+                    }
                 }
+                if (find) break;
             }
 
-            // if no contour found
-            if (biggestContourIdx < 0)
-            {
-                std::cout << "no contour found" << std::endl;
-                return 1;
-            }
+            Cv2.Rectangle(image, plate, new Scalar(255, 255, 255), -1);
+            pictureBox1.Image = new Bitmap (OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image));
+            Cv2.ImWrite(@"D:\Visual_studio\RegistrationPlateRecognizer\Pictures\hidePlate.jpg", image);
 
-            // compute the rotated bounding rect of the biggest contour! (this is the part that does what you want/need)
-            cv::RotatedRect boundingBox = cv::minAreaRect(contours[biggestContourIdx]);
-            // one thing to remark: this will compute the OUTER boundary box, so maybe you have to erode/dilate if you want something between the ragged lines
+            /*
+            
 
+             // draw the rotated rect
+             cv::Point2f corners[4];
+             boundingBox.points(corners);
+             cv::line(drawing, corners[0], corners[1], cv::Scalar(255, 255, 255));
+             cv::line(drawing, corners[1], corners[2], cv::Scalar(255, 255, 255));
+             cv::line(drawing, corners[2], corners[3], cv::Scalar(255, 255, 255));
+             cv::line(drawing, corners[3], corners[0], cv::Scalar(255, 255, 255));
 
+             // display
+             cv::imshow("input", input);
+             cv::imshow("drawing", drawing);
+             cv::waitKey(0);
 
-            // draw the rotated rect
-            cv::Point2f corners[4];
-            boundingBox.points(corners);
-            cv::line(drawing, corners[0], corners[1], cv::Scalar(255, 255, 255));
-            cv::line(drawing, corners[1], corners[2], cv::Scalar(255, 255, 255));
-            cv::line(drawing, corners[2], corners[3], cv::Scalar(255, 255, 255));
-            cv::line(drawing, corners[3], corners[0], cv::Scalar(255, 255, 255));
+             cv::imwrite("rotatedRect.png", drawing);
 
-            // display
-            cv::imshow("input", input);
-            cv::imshow("drawing", drawing);
-            cv::waitKey(0);
+     */
 
-            cv::imwrite("rotatedRect.png", drawing);
-            //OpenCvSharp.Point[][] contours = Cv2.FindContoursAsArray(WhiteImage, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
-
-
-            Cv2.ImShow("kép", WhiteImage);
         }
 
         void Dilate(Mat image, int number)
@@ -102,14 +102,17 @@ namespace RegistrationPlateRecognizer
             }
         }
 
-        private void cannyButton_Click(object sender, EventArgs e)
+        void Erode(Mat image, int number)
         {
-            Cv2.NamedWindow("canny");
-            Cv2.ResizeWindow("canny", 1280, 720);
-            canny = new Mat();
-            Cv2.Canny(WhiteImage, canny, 1000, 500);
-            Dilate(canny, 1);
-            Cv2.ImShow("canny", canny);
+            for (int i = 0; i < number; i++)
+            {
+                Cv2.Erode(image, image, new Mat());
+            }
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(path);
         }
     }
 }
